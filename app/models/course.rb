6d3048d1120e3d101032b9e4d38dda8bb67e3f07
nijeesh <nijeesh4all@gmail.com>
2022-09-15 20:11:43 +0530

    added validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
   has_many :tutors, dependent: :destroy
   accepts_nested_attributes_for :tutors, allow_destroy: :true
+
+  validates_presence_of :name
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,5 @@
 class Tutor < ApplicationRecord
   belongs_to :course
+
+  validates_presence_of :name
 end
